0615c244b5578c13b0c632981f7a29282d8cbce3
Amarsah15 <gsah150803@gmail.com>
2026-04-14 11:27:55 +0530

    fix: update API base URL to production endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const resolvedBaseUrl = "http://127.0.0.1:8000" ;
-const resolvedBaseUrl = "http://127.0.0.1:8000" ;
+const resolvedBaseUrl = "https://smart-video-summarizer-cjst.onrender.com";
 
 const api = axios.create({
   baseURL: resolvedBaseUrl,
